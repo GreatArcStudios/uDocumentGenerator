2c0114618f38512c50acf096d87ea449968b7aa2
Eric Zhu <ericz2241@gmail.com>
2019-07-26 13:07:28 -0400

    Fixed some mobile formatting issues

---
+++ docs/precache-manifest.f752fb8cf0798ea385354dc4c1b70962.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3389f86b86bd3367426eb0eb418d61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70456b4e9ec70942bde8",
+    "url": "./static/css/2.842d2311.chunk.css"
+  },
+  {
+    "revision": "c159ab575442d1915863",
+    "url": "./static/css/main.f1654791.chunk.css"
+  },
+  {
+    "revision": "70456b4e9ec70942bde8",
+    "url": "./static/js/2.adde08b0.chunk.js"
+  },
+  {
+    "revision": "c159ab575442d1915863",
+    "url": "./static/js/main.1dfeb001.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8145f90be13b40d315047c92c501f6a9",
+    "url": "./static/media/clear.8145f90b.svg"
+  },
+  {
+    "revision": "d73c60b9d4d8bfd7d4a250883d4e478f",
+    "url": "./static/media/search.d73c60b9.svg"
+  },
+  {
+    "revision": "5e108109586ec4159b8b2a0154b4b144",
+    "url": "./static/media/uDG.5e108109.png"
+  }
+]);
