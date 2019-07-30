50c950e11dfad6db94192d0dcec0d1c4532290b5
Eric Zhu <ericz2241@gmail.com>
2019-07-29 22:15:54 -0400

    Small mobile fixes - almost release ready! 🔨

---
+++ docs/precache-manifest.f08fcaf26cbcd8ce1017d332dbdf3e81.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb94e7e974a7654c1265d187d444a38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81c4fdee7bc20d932cf0",
+    "url": "./static/css/2.a2d610b9.chunk.css"
+  },
+  {
+    "revision": "8ea3ca629b8a5c5d7053",
+    "url": "./static/css/main.9296d283.chunk.css"
+  },
+  {
+    "revision": "81c4fdee7bc20d932cf0",
+    "url": "./static/js/2.5cd9f0ac.chunk.js"
+  },
+  {
+    "revision": "8ea3ca629b8a5c5d7053",
+    "url": "./static/js/main.b2cd08a3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8145f90be13b40d315047c92c501f6a9",
+    "url": "./static/media/clear.8145f90b.svg"
+  },
+  {
+    "revision": "d73c60b9d4d8bfd7d4a250883d4e478f",
+    "url": "./static/media/search.d73c60b9.svg"
+  },
+  {
+    "revision": "5e108109586ec4159b8b2a0154b4b144",
+    "url": "./static/media/uDG.5e108109.png"
+  }
+]);
